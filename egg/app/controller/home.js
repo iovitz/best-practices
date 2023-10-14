0dcb2ecbc1576b7bc12e14e2db58e33a0fa08920
iovitz <iovitz1999@gmail.com>
2023-10-14 08:54:50 +0800

    feat: 优化接口注释

--- egg/app/controller/home.js
+++ egg/app/controller/home.js
@@ -9,9 +9,9 @@ class HomeController extends Controller {
   /**
    * @summary 获取服务器状态
    * @description Get请求获取服务器状态
-   * @router get /status （ get 表示设置请求为 get 请求，最后的 selectById 对应下面的 selectById 方法 ）。
-   * @request query integer Id 需要去查新的ID。（ get 对应 query 请求，请求值设定为 integer 纯数字类型，ID 为请求的字段，注意大小写，和下面的方法要一一对应，不然会报错 ）。
-   * @response 200 getStatusResponse 返回结果。（ 对应 contract 里面的验证属性，下面会提到 。）
+   * @router get /status
+   * @request query integer Id 查询的id
+   * @response 200 getStatusResponse 返回结果
    */
   getStatus() {
     return this.ctx.success("Server Ready, Your params is 1");
