6dc794c4d2514f296d902381a6f5f1d87f5de132
iovitz <iovitz1999@gmail.com>
2023-12-10 10:23:15 +0800

    chore: 优化egg

--- egg/app/controller/home.js
+++ egg/app/controller/home.js
@@ -1,33 +1,8 @@
-"use strict";
-
 const Controller = require("../core/controller");
 
-/**
- * @controller HomeController 根目录接口
- */
 class HomeController extends Controller {
-  /**
-   * @summary 获取服务器状态
-   * @description Get请求获取服务器状态
-   * @router get /status
-   * @request query integer Id 查询的id
-   * @response 200 getStatusResponse 返回结果
-   */
   getStatus() {
-    return this.ctx.success("Server Ready, Your params is 1");
-  }
-
-  /**
-   * @summary 获取服务器状态
-   * @description Post请求获取服务器状态
-   * @Router POST /status
-   * * @request body postStatusBody body
-   * @Response 200 postStatusResponse ok
-   */
-  async postStatus() {
-    const { ctx, app } = this;
-    ctx.validate(ctx.rule.postStatusBody);
-    return this.ctx.success("Server Ready, Your params is 1");
+    return this.ctx.success("Hello");
   }
 }
 
