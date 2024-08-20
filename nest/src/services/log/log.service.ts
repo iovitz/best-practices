d2605d177bf5faeb41d2c3e8140371943a094b56
iovitz <iovitz1999@gmail.com>
2024-08-20 21:13:40 +0800

    feat: 替换日志库为pino

--- nest/src/services/log/log.service.ts
+++ nest/src/services/log/log.service.ts
@@ -1,133 +1,36 @@
 import { Injectable, LoggerService } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as chalk from 'chalk';
-import { Logger, createLogger, format, transports } from 'winston';
-import { SPLAT } from 'triple-beam';
-import 'winston-daily-rotate-file';
-import { isEmpty, isObject } from 'lodash';
-
-type Format = ReturnType<typeof format.timestamp>;
+import pino, { Logger } from 'pino';
 
 @Injectable()
 export class LogService implements LoggerService {
-  private logger: Logger;
-
+  logger: Logger;
   constructor(private config: ConfigService) {
-    this.logger = createLogger({
-      transports: [
-        this.getConsoleTransport(),
-        this.getInfoTransport(),
-        this.getErrorTransport(),
-      ],
+    this.logger = pino({
+      // 配置 Pino
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'SYS:HH:mm:ss.l',
+        },
+      },
     });
-  }
-
-  formatObject(param: unknown, indent = false) {
-    if (param instanceof Error) {
-      return param.stack.split('\n').join('\\n');
-    }
-    if (isObject(param)) {
-      if (indent) {
-        return JSON.stringify(param, null, 2);
-      }
-      return JSON.stringify(param);
-    }
-    return param;
-  }
-
-  formatInfo(info, indent = false): Format {
-    const splat = info[SPLAT] || [];
-    const message = this.formatObject(info.message, indent);
-    const rest = splat.map(this.formatObject).join(' \\n ');
-
-    info.message = `${message}`;
-    if (!isEmpty(rest)) {
-      info.message += ` ${rest}`;
-    }
-    return info;
-  }
-
-  getConsoleTransport() {
-    return new transports.Console({
-      level: 'debug',
-      // 使用时间戳和nest样式
-      format: format.combine(
-        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
-        format.printf((i) => {
-          this.formatInfo(i);
-          console.log(i);
-          const t = chalk.gray(i.timestamp);
-          const message = chalk.blue(i.message);
-          return `${[t]} ${i.level} ${message} ${i.context || ''}`;
-        }),
-      ),
-    });
-  }
-
-  getInfoTransport() {
-    return new transports.DailyRotateFile({
-      dirname: 'logs/info',
-      filename: '%DATE%.log',
-      datePattern: 'YYYY-MM-DD',
-      zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
-      level: 'info',
-      format: format.combine(
-        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
-        format.printf((i) => {
-          this.formatInfo(i);
-          const t = i.timestamp;
-          const { message } = i;
-          return `${[t]} ${i.level} ${message} ${i.context || ''}`;
-        }),
-      ),
-    });
-  }
-
-  getErrorTransport() {
-    return new transports.DailyRotateFile({
-      dirname: 'logs/error',
-      filename: '%DATE%.log',
-      datePattern: 'YYYY-MM-DD',
-      zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
-      level: 'error',
-      format: format.combine(
-        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
-        format.printf((i) => {
-          this.formatInfo(i);
-          const t = i.timestamp;
-          const { message } = i;
-          return `${[t]} ${i.level} ${message} ${i.context || ''}`;
-        }),
-      ),
-    });
-  }
-
-  formatContext(context: unknown) {
-    if (context !== null && typeof context === 'object') {
-      return {
-        ...context,
-      };
-    }
-    return context;
   }
 
   log(message: any, context?: unknown) {
-    this.logger.info(message, this.formatContext(context));
+    this.logger.info(message, context);
   }
   error(message: any, context?: unknown) {
-    this.logger.error(message, this.formatContext(context));
+    this.logger.error(message, context);
   }
   warn(message: any, context?: unknown) {
-    this.logger.warn(message, this.formatContext(context));
+    this.logger.warn(message, context);
   }
   debug(message: any, context?: unknown) {
-    this.logger.debug(message, this.formatContext(context));
+    this.logger.debug(message, context);
   }
-  verbose(message: any, context?: unknown) {
-    this.logger.verbose(message, this.formatContext(context));
+  verbose(message: any, context?: string) {
+    this.logger.info(message, { context });
   }
 }
