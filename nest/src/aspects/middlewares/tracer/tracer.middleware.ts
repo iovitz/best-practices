4472e5814cc4fcbbe2df06d51d502c23f5b05617
iovitz <iovitz1999@gmail.com>
2024-08-22 16:07:04 +0800

    feat: 优化请求日志

--- nest/src/aspects/middlewares/tracer/tracer.middleware.ts
+++ nest/src/aspects/middlewares/tracer/tracer.middleware.ts
@@ -10,17 +10,26 @@ export class TracerMiddleware implements NestMiddleware {
   async use(req: Req, res: Res, next: () => void) {
     const stime = process.hrtime.bigint();
     req.stime = stime;
-    const { method, path } = req;
+    const { method, originalUrl } = req;
     const requestTid = res.get('tracer-id');
     const rid = requestTid || `${Date.now()}${this.tracerIdGenerator()}`;
     const userId = req.session.userId;
     const requestLogger = this.log.child({
       traceInfo: `${rid}${userId ? `#${userId}` : ''}`,
     });
+    res.on('finish', function (this: Res) {
+      const cost = process.hrtime.bigint() - stime;
+      requestLogger.log('REQUEST FINISH', {
+        cost: cost.toString(),
+        status: this.statusCode,
+      });
+    });
 
-    requestLogger.log(`reqMeta：${userId} ${method} ${path}`);
+    requestLogger.log(
+      `Incoming Info：${userId ?? 'NO_USER'} ${method} ${originalUrl}`,
+    );
     // 生产环境不上报
-    requestLogger.debug('reqData', {
+    requestLogger.debug('Incoming Data', {
       body: req.body,
       query: req.query,
       params: req.params,
