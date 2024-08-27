7c7e236def1b84add6c440f457fd818a5f956944
iovitz <iovitz1999@gmail.com>
2024-08-27 19:17:42 +0800

    fix: 修复exception filter中没有打印tracer id 的问题

--- nest/src/aspects/filters/http-exception/http-exception.filter.ts
+++ nest/src/aspects/filters/http-exception/http-exception.filter.ts
@@ -5,30 +5,29 @@ import {
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { TracerService } from 'src/services/tracer/tracer.service';
 import * as statuses from 'statuses';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-  constructor(
-    private tracer: TracerService,
-    private config: ConfigService,
-  ) {}
+  constructor() {}
 
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const res = ctx.getResponse<Res>();
+    const req = ctx.getRequest<Req>();
     const status =
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    this.tracer.debug(`HttpExceptionFilter${status}`, exception.message);
+    if (status < 500) {
+      req.tracer.log(`Client Error(${status})`, exception.message);
+    } else {
+      req.tracer.warn(`Server Error(${status})`, exception);
+    }
 
-    const message = this.config.getOrThrow('isOnline')
-      ? statuses(status)
-      : exception.message;
+    // 不把详细的错误码吐给前端
+    const message = statuses(status);
 
     const errorResponse = {
       code: status * 100,
