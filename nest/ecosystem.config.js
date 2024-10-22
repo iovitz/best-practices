74708eed1e77e381c9ce1f9583c40f2ce3d6219d
iovitz <iovitz1999@gmail.com>
2024-10-22 13:20:21 +0800

    chore: 优化部署脚本，节约内存

--- nest/ecosystem.config.js
+++ nest/ecosystem.config.js
@@ -2,8 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'nest-app',
-      script: 'npm',
-      args: 'start:prod',
+      exec_interpreter: 'node',
+      script: './dist/src/main.js',
+      // 不要使用npm的方式，否则会多出一个npm start的进程
+      // script: 'npm',
+      // args: ['run', 'start:prod'],
       exec_mode: 'fork',
       instances: 1,
       autorestart: true,
