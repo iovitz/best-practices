9756952fea513be3837b52edf4ff6a6974da8cc9
iovitz <iovitz1999@gmail.com>
2024-11-08 19:00:04 +0800

    feat: 使用mimetype

--- nest/src/home/home.controller.ts
+++ nest/src/home/home.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Header, Render } from '@nestjs/common'
+import { contentType } from 'mime-types'
 import { HeaderKeys } from 'src/shared/constans/header'
 
 @Controller()
 export class HomeController {
   @Get()
   @Render('index')
-  @Header(HeaderKeys.ContentType, 'text/html;charset=urf-8')
+  @Header(HeaderKeys.ContentType, contentType('html') as string)
   getIndex() {
     return {
       message: 'Hello, world!!!',
