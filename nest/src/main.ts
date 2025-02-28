084f6b786c980e355c2c6d1c1a26ea833b50f4ef
iovitz <iovitz1999@gmail.com>
2025-02-28 18:09:44 +0800

    feat: 增加启动出错的日志

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -9,27 +9,31 @@ import { appLogger, formatLogContext } from './services/tracer/tracer'
 import { Tracer } from './services/tracer/tracer.service'
 import { RcConfig } from './shared/config'
 
-// 防止未捕获异常导致进程退出
-process.on('unhandledRejection', (reason: Error) => {
-  appLogger.error('###Unhandle Rejection Promise', formatLogContext(reason))
+const appTracer = new Tracer('APP')
+
+appTracer.error('Application Running', {
+  version: pkg.version,
+  env: JSON.stringify(RcConfig),
 })
 
+// 防止未捕获异常导致进程退出
+process.on('unhandledRejection', (reason: Error) => {
+  appTracer.error('### Unhandle Rejection Promise', reason)
+})
+
+process.on('uncaughtException', (error) => {
+  appTracer.error('### Unhandle Exception', error)
+})
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    // 先不打印日志，放入缓冲区，直到指定了Logger才进行打印
-    bufferLogs: true,
+    logger: appTracer,
+    abortOnError: false,
   })
-
-  const appTracer = new Tracer('APP')
-
-  app.useLogger(appTracer)
 
   appTracer.log('Application Running', {
     version: pkg.version,
     env: JSON.stringify(RcConfig),
   })
-
-  const configService = app.get(ConfigService)
 
   app.useStaticAssets('public', {
     // 虚拟路径为 static
@@ -56,10 +60,13 @@ async function bootstrap() {
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
   SwaggerModule.setup('doc', app, swaggerDocument)
 
-  const appPort = configService.get('APP_PORT')
+  const appPort = RcConfig.APP_PORT
   await app.listen(appPort)
 
   appTracer.log(`Server running in http://127.0.0.1:${appPort}`)
 }
 
-bootstrap()
+bootstrap().catch((e) => {
+  appTracer.error('### APP Exit!!!!', e)
+  process.exit(1)
+})
