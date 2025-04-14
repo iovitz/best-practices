1394e6a8d1219993869b5917912e18a880123e95
iovitz <iovitz1999@gmail.com>
2025-04-14 17:45:30 +0800

    feat: 流失返回示例

--- nest/src/futures/home/home.controller.ts
+++ nest/src/futures/home/home.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Header, Render } from '@nestjs/common'
+import { Controller, Get, Header, Render, Res } from '@nestjs/common'
 import { contentType } from 'mime-types'
 import { HeaderKeys } from 'src/shared/constans/header'
 
@@ -11,5 +11,38 @@ export class HomeController {
     return {
       message: 'Hello, world!!!',
     }
+  }
+
+  @Get('stream')
+  streamTemplate(@Res() res: Res) {
+    res.setHeader('Content-Type', 'text/html')
+
+    // 第一部分
+    res.write(`
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Streamed Page</title>
+      </head>
+      <body>
+        <h1>Header</h1>
+    `)
+
+    // 第二部分（延迟发送）
+    setTimeout(() => {
+      res.write(`
+        <h1>Main</h1>
+      `)
+    }, 1000)
+
+    // 第三部分（结束）
+    setTimeout(() => {
+      res.write(`
+        <h1>Footer</h1>
+      </body>
+      </html>
+      `)
+      res.end()
+    }, 2000)
   }
 }
